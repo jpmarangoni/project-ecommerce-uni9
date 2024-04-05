bcf081401ffa415c7e829a143ac9c3ed4996d2e5
jpmarangoni <joao200213pedro@gmail.com>
2024-04-05 18:07:52 -0300

    [FEAT] - First commit

---
+++ src/main/java/com/uni9/projectecommerceuni9/ProjectEcommerceUni9Application.java
@@ -0,0 +1,13 @@
+package com.uni9.projectecommerceuni9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEcommerceUni9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectEcommerceUni9Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/uni9/projectecommerceuni9/ProjectEcommerceUni9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni9.projectecommerceuni9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEcommerceUni9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
