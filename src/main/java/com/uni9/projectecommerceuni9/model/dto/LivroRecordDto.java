6917b21e31d7219f57eb61d2feb0f1b3cf09fd4b
jpmarangoni <joao200213pedro@gmail.com>
2024-04-12 21:18:30 -0300

    [FEAT] - Adicionando DTO do Livro

---
+++ src/main/java/com/uni9/projectecommerceuni9/model/dto/LivroRecordDto.java
@@ -0,0 +1,15 @@
+package com.uni9.projectecommerceuni9.model.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record LivroRecordDto(
+        @NotBlank String titulo,
+        @NotBlank String linkImagem,
+        @NotBlank String numeroPaginas,
+        @NotBlank String idioma,
+        @NotBlank String editora,
+        @NotBlank String autor,
+        @NotBlank String descricao,
+        @NotBlank String anoEdicao,
+        @NotBlank Double preco
+) {}
